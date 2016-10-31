6f67430f45ee0bbdbf8d7cd99cd2e905a3d0b03c
sruthi2498 <vsruthi98@gmail.com>
2016-10-31 12:12:03 +0530

    Add files via upload

--- yourpage.php
+++ yourpage.php
@@ -1,31 +1,26 @@
-<?php
-session_start();
-?>
+<!DOCTYPE html>
 <html>
-	<head>
-		
-	</head>
-	<body>
-		<?php
-        //echo "hello";
-      
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+    session_start();
+    extract ($_SESSION);
     $servername = "localhost";
     $username = "root";
     $password = "charu1706";
     $conn = mysqli_connect($servername, $username, $password);
     //$sql = "CREATE DATABASE myDB";
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-     //session_start();
-   extract ($_SESSION);
-   //echo "username : ".$_SESSION["username"]." password : ".$SESSION["pswd"]."<br>";
-    /*$sql12=mysqli_select_db($conn,'myDB');
-    $i=1;*/
-   $n=$_SESSION["username"];
-    $p=$_SESSION["pswd"]; 
-    echo $n." ".$p;
-    ?>
-	</body>
+    $sql12=mysqli_select_db($conn,'myDB');
+    $n=$_SESSION["username"];
+    $p=$_SESSION["pswd"];
+    echo "<div class='1'>"."hello"."</div>";
+    
+?>
+ 
+
+
+
+</body>
 </html>
