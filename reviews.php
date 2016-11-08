cee3b206426ecf2489ad0e3e4fa53fcd3a0f8a92
VibhaSatya <vibhavarna@gmail.com>
2016-11-08 20:08:46 +0530

    Add files via upload

--- reviews.php
+++ reviews.php
@@ -6,9 +6,9 @@
 </head>
 <body style="background-color:black">
 <form method="post" action="reviews.php" style="position:relative; top:100px;">
-<input type="text" name="comment" id="comment" placeholder="Type here" onfocus="f(this)" onblur="b(this)" style="width:700px;height:100px">
+<input type="text" id="comment" placeholder="Type here" onfocus="f(this)" onblur="b(this)" style="width:700px;height:100px">
 <br><br>
-<input type="submit" value= "Add Review">
+<input type="button" value= "Add Review" onclick="add()">
 </form>
 <script>
 function f(x)
@@ -21,19 +21,12 @@ x.placeholder="Type here";
 }
 </script>
 <?php
-echo "here";
 $file=fopen("review.txt","a");
-extract($_POST);
-$text=$_POST["comment"]."<br><br><br>";
-fwrite($file,$text);
-fclose($file);
-$file=fopen("review.txt","r");
-echo "<br><br><br><br><br><br><br>";
-echo "<div style='color:white;fontStyle:italic'>";  
-$line=fgets($file);
-echo "$line";
-fclose($file);
-echo "</div>";
+
+
+
+
+
 ?>
 
 </body>
